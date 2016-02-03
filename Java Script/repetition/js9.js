33f8a28e4d9f027e9528e67b32eb9310d4fae52e
KristinaShh <kshalkoska@gmail.com>
2016-02-03 16:39:40 +0100

    Simple Slider

--- Java Script/repetition/js9.js
+++ Java Script/repetition/js9.js
@@ -4,9 +4,24 @@ var ul = document.querySelector('.wrap ul');
 
 var margin = 0;
 
-btnLeft.addEventListener('click', function(){
-    if(margin > - calc(88.88%)){
-        margin = margin - 11.11%;
+//btnLeft.addEventListener('click', function(){
+//    if(margin > - 166.66 ){
+//        margin = margin - 33.33;
+//        ul.style.marginLeft = margin + '%';
+//    }
+//    //if(margin < 0 ){
+//    //    margin = margin + 33.33;
+//    //    ul.style.marginLeft = margin + '%';
+//    //}
+//});
+
+
+setInterval(function(){
+    if(margin > -166.66){
+        margin = margin - 33.33;
+        ul.style.marginLeft = margin + '%';
+    }else{
+        margin = 0;
         ul.style.marginLeft = margin + '%';
     }
-});
+}, 5000);
