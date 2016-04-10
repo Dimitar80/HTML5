ac5a90d918207f47d5944ad0c8a98a335dd8db68
KristinaShh <kshalkoska@gmail.com>
2016-04-10 20:25:25 +0200

    update

--- 22Oktomvri/assets/scripts/gallery.js
+++ 22Oktomvri/assets/scripts/gallery.js
@@ -1,13 +1,14 @@
-/**
- * Created by Kristina Shalkoska on 4/10/2016.
- */
-   //$(document).ready(function () {
-   //     $("#nanoGallery3").nanoGallery({
-   //         thumbnailWidth: 'auto',
-   //         thumbnailHeight: 250,
-   //         locationHash: false,
-   //         thumbnailHoverEffect:'borderLighter,imageScaleIn80',
-   //         itemsBaseURL:'http://nanogallery.brisbois.fr/demonstration/'
-   //     });
-   // });
-   //
+///**
+// * Created by Kristina Shalkoska on 4/10/2016.
+// */
+
+$(document).ready(function () {
+    $("#nanoGallery3").nanoGallery({
+        thumbnailWidth: 'auto',
+        thumbnailHeight: 250,
+        locationHash: false,
+        thumbnailHoverEffect:'borderLighter,imageScaleIn80',
+        itemsBaseURL:'http://nanogallery.brisbois.fr/demonstration/'
+    });
+});
+
