912eafe7167b2d7e70901a557f11d7b32ee81622
KristinaShh <kshalkoska@gmail.com>
2016-05-25 22:54:27 +0200

    gallery

--- Portfolio PSD to HTML/assets/scripts/global.js
+++ Portfolio PSD to HTML/assets/scripts/global.js
@@ -1,10 +1,16 @@
-/**
- * Created by Kristina Shalkoska on 4/10/2016.
- */
+
 $(document).ready(function(){
   
-//smooth scroll code
+    //smooth scroll code
     $('a').smoothScroll({
         speed:800
     });
+
+    <!-- Kick off Filterizr -->
+    $(function() {
+        //Initialize filterizr with default options
+        $('.filtr-container').filterizr();
+    });
+
+
 });
